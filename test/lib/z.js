ddc77175e603e433d12fd537930d8b5696436715
Andrew Vayanis <avayanis@riotgames.com>
2014-01-27 01:28:51 -0800

    Updating zzz unit tests for connect.

--- test/lib/z.js
+++ test/lib/z.js
@@ -1,18 +1,31 @@
-require("./../test");
+require('./../test');
 
-var z = require(libpath + "/z");
+var Zzz = require(libpath + '/z');
 
-describe("Zzz Module", function() {
+describe('Zzz Module', function() {
 
-    describe("z.createServer", function() {
+  describe('Constructor', function() {
 
-        it("Should have a createServer function", function() {
+    var server;
 
-            z.should.have.ownProperty("createServer");
-            z.createServer.should.be.a("function");
-
-        });
-
+    beforeEach(function() {
+      server = new Zzz();
     });
 
+    it('Should have standard REST methods', function() {
+
+      expect(server).to.have.property('get');
+      expect(server).to.have.property('post');
+      expect(server).to.have.property('put');
+      expect(server).to.have.property('delete');
+    });
+  });
+
+  describe('Module', function(){
+
+    it('Should have connect middleware available', function() {
+
+      expect(Zzz).to.have.property('logger');      
+    });
+  });
 });
