8143d014d6a4b33af7b74009eaf627776666d3f2
Andrew Vayanis <avayanis@riotgames.com>
2014-01-27 01:34:47 -0800

    Updating formatting and adding new versions of Node.js for testing.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ expect = chai.expect;
 assert = chai.assert;
 
 if (process.env.HTML_COVERAGE) {
-    libpath = __dirname + "/../lib-cov"
+    libpath = __dirname + '/../lib-cov'
 } else {
-    libpath = __dirname + "/../lib"
+    libpath = __dirname + '/../lib'
 }
